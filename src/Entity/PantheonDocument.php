2bd47ac20717ca8b88882ceb34be3b552058c097
Roshny Kunjappan <roshnykunjappan@pantheon.io>
2025-09-14 23:47:49 -0400

    fix syntax error

--- src/Entity/PantheonDocument.php
+++ src/Entity/PantheonDocument.php
@@ -129,7 +129,7 @@ class PantheonDocument extends ContentEntityBase implements PantheonDocumentInte
 
     $fields['changed'] = BaseFieldDefinition::create('changed')
       ->setLabel(t('Changed'))
-      ->setDescription(t('The time that the pantheon content publisher was last edited.'));
+      ->setDescription(t('The time that the pantheon content publisher was last edited.'))
       ->setDisplayOptions('view', [
         'region' => 'hidden',
       ])
