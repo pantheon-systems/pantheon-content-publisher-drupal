213ab2261b4b62a64cc30fba1dea7f8e601fb4ed
Malik Kotob <malikkotob@users.noreply.github.com>
2025-11-01 22:10:20 -0400

    [SITE-5342] Add proper publish and unpublish support. (#32)

--- src/PantheonDocumentInterface.php
+++ src/PantheonDocumentInterface.php
@@ -6,10 +6,21 @@ namespace Drupal\pantheon_content_publisher;
 
 use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Entity\EntityChangedInterface;
+use Drupal\Core\Entity\EntityPublishedInterface;
 
 /**
  * Provides an interface defining a pantheon content publisher entity type.
  */
-interface PantheonDocumentInterface extends ContentEntityInterface, EntityChangedInterface {
+interface PantheonDocumentInterface extends ContentEntityInterface, EntityChangedInterface, EntityPublishedInterface {
+
+  /**
+   * Denotes that the document is not published.
+   */
+  const NOT_PUBLISHED = 0;
+
+  /**
+   * Denotes that the document is published.
+   */
+  const PUBLISHED = 1;
 
 }
